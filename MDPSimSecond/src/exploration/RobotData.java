2b16178723094a08dd4256491ec8cde6969214cc
Babein <xuyaohe.bessie@163.com>
2015-09-07 20:06:49 +0800

    delete

--- MDPSimSecond/src/exploration/RobotData.java
+++
@@ -1,9 +0,0 @@
-package exploration;
-
-public interface RobotData {
-	//the 4 directions that the robot could face
-	static final int EAST = 1;
-	static final int SOUTH = 2;
-	static final int WEST = 3;
-	static final int NORTH = 4;
-}
